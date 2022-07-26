95a37fd1985a483f184a4871d7b2d8e546aafedb
Breno Stacheski <brenosttc@gmail.com>
2022-07-25 21:03:27 -0300

    inserção de debugbar + criação controller seasons

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Models\Serie;
+use App\Models\Episode;
+use App\Models\Season;
+use App\Models\Series;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
@@ -12,7 +14,7 @@ class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = Serie::query()->get();
+        $series = Series::query()->get();
         $mensagemSucesso = session('mensagem.sucesso');
 
         return view('series.index')->with('series', $series)
@@ -27,25 +29,45 @@ class SeriesController extends Controller
     public function store(SeriesFormRequest $request)
     {
         $nome = $request->nome;
-        $serie = new Serie();
+        $serie = new Series();
         $serie->nome = $nome;
         $serie->save();
+        $seasons = [];
 
+        for ($i = 1; $i <= $request->seasonsQty; $i++) {
+            $seasons[] = [
+                'series_id' => $serie->id,
+                'number' => $i,
+            ];
+        }
+        Season::insert($seasons);
+
+        $episodes = [];
+        foreach ($serie->seasons as $season) {
+            for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
+                $episodes[] = [
+                    'season_id' => $season->id,
+                    'number' => $j
+                ];
+            }
+        }
+        Episode::insert($episodes);
+    
         return Redirect::to('series')
         ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso!");
     }
 
     public function destroy(Request $request, $id)
     {
-        $serie = Serie::where('id', $id)->first();
+        $serie = Series::where('id', $id)->first();
         $serie->delete();
 
-        return view('series.index', ['mensagemSucesso' => "Série '{$serie->nome}' removida com sucesso", 'series' => Serie::all()]);
+        return view('series.index', ['mensagemSucesso' => "Série '{$serie->nome}' removida com sucesso", 'series' => Series::all()]);
     }
 
     public function edit(Request $request, $id)
     {
-        $serie = Serie::where('id', $id)->first();
+        $serie = Series::where('id', $id)->first();
         if (empty($serie)) {
             return "Série inexistente";
         } else {
@@ -55,13 +77,13 @@ class SeriesController extends Controller
 
     public function update(SeriesFormRequest $request, $id)
     {
-        $serie = Serie::where('id', $id)->first();
+        $serie = Series::where('id', $id)->first();
         if (empty($serie)) {
             return "Série inexistente";
         } else {
             $serie->nome = $request->nome;
             $serie->save();
-            return view('series.index')->with('series', Serie::all());
+            return view('series.index')->with('series', Series::all());
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SeriesController;
+use App\Http\Controllers\SeasonsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,13 +19,14 @@ Route::get('/', function () {
     return redirect('/series');
 });
 
-Route::controller(SeriesController::class)->group(function () {
+Route::controller(SeriesController::class, SeasonsController::class)->group(function () {
     Route::get('/series', 'App\Http\Controllers\SeriesController@index');
     Route::get('/series/criar', 'App\Http\Controllers\SeriesController@create');
     Route::post('/series/salvar', 'App\Http\Controllers\SeriesController@store');
     Route::get('/series/editar/{id}', 'App\Http\Controllers\SeriesController@edit');
     Route::get('/series/atualizar/{id}', 'App\Http\Controllers\SeriesController@update');
     Route::delete('/series/destroy/{id}', 'App\Http\Controllers\SeriesController@destroy')->name('series.destroy');
+    Route::get('/series/{series}/seasons', 'App\Http\Controllers\SeasonsController@index')->name('seasons.index');
 });
 
 //Route::put('/series/editar/{id}', 'App\Http\Controllers\SeriesController@edit');
